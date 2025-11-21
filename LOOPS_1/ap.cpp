9932cc73d10b2d0c131d222c2ffc774c614a1a49
Danish956 <mddanish1222001@gmail.com>
2025-11-21 22:22:00 +0530

    added

---
+++ LOOPS_1/ap.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int n;
+   cout<<"eneter the number of term";
+   cin>>n;
+//    for(int i=5;i<=3*n+2;i+=3){
+//         cout<<i<<endl;
+//    }
+
+int a=5;
+for(int i=1; i<=n; i++){
+    cout<<a<<endl;
+    a*=3;
+}
+
+}
